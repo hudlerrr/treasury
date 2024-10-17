d6dbc8d682636135714ae5d253020c859571e550
Bilal (Birm) Wais <b.wais@outlook.com>
2024-10-17 16:55:34 -0700

    add google sign in provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <pre>
-            <code>{JSON.stringify(contract, null, 2)}</code>
+            {/* <code>{JSON.stringify(contract, null, 2)}</code> */}
           </pre>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link

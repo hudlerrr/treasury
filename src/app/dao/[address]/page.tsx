17f55bde2aa42c0de10f6614932322d83abf193e
Bilal (Birm) Wais <b.wais@outlook.com>
2024-10-23 20:40:31 -0600

    display with aave example

--- src/app/dao/[address]/page.tsx
+++ src/app/dao/[address]/page.tsx
@@ -1,8 +1,16 @@
-export default function Dao(props: { params: { address: string } }) {
+import { api } from "@/trpc/server";
+
+export default async function Dao(props: { params: { address: string } }) {
+  // Example aave.eth address look up
+  const dao = await api.safe.getBalance({
+    address: "0xC77624e672E762907435725Bbd3dFe70DDD40D13",
+  });
+
   return (
     <div>
       <h1>DAO</h1>
       <p>{props.params.address}</p>
+      <pre>{JSON.stringify(dao, null, 2)}</pre>
     </div>
   );
 }

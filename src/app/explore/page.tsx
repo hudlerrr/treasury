50070ed4af6169362585df885fb7294974eb5c8e
Bilal (Birm) Wais <b.wais@outlook.com>
2024-10-28 11:15:15 -0600

    fix explore grid layout

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -6,57 +6,49 @@ export default async function Explore() {
   return (
     <div>
       <h1>Explore</h1>
-      <div className="flex flex-row flex-wrap gap-4">
+      <ul className="faded-bottom no-scrollbar grid gap-4 overflow-auto pb-16 pt-4 md:grid-cols-3 lg:grid-cols-4">
         {daos.map((dao) => (
-          <a href={dao.href} className="" key={dao.href}>
-            <div
-              className="border-skin-border bg-skin-block-bg hover:border-skin-text mb-0 flex items-center justify-center border-y text-center text-base transition-all md:rounded-xl md:border"
-              style={{ height: "266px" }}
-            >
-              <div className="break-words p-4 leading-5 sm:leading-6">
-                <div className="relative mb-2 inline-block">
-                  <div symbol-index="space" className="mb-1">
-                    <img
-                      className="bg-skin-border rounded-full object-cover"
-                      alt="avatar"
-                      style={{
-                        width: "82px",
-                        height: "82px",
-                        minWidth: "82px",
-                        display: "none",
-                      }}
-                    />
-                    <img
-                      src={dao.avatarSrc}
-                      className="bg-skin-border rounded-full object-cover"
-                      alt="avatar"
-                      style={{
-                        width: "82px",
-                        height: "82px",
-                        minWidth: "82px",
-                      }}
-                    />
+          <li key={dao.href}>
+            <a href={dao.href} className="" key={dao.href}>
+              <div
+                className="border-skin-border bg-skin-block-bg hover:border-skin-text mb-0 flex items-center justify-center border-y text-center text-base transition-all hover:shadow-md md:rounded-xl md:border"
+                style={{ height: "266px" }}
+              >
+                <div className="break-words p-4 leading-5 sm:leading-6">
+                  <div className="relative mb-2 inline-block">
+                    <div symbol-index="space" className="mb-1">
+                      <img
+                        src={dao.avatarSrc}
+                        className="bg-skin-border rounded-full object-cover"
+                        alt="avatar"
+                        style={{
+                          width: "82px",
+                          height: "82px",
+                          minWidth: "82px",
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="flex items-center justify-center gap-1 truncate">
-                  <h3 className="mb-0 mt-0 !h-[32px] overflow-hidden pb-0 text-[22px]">
-                    {dao.name}
-                  </h3>
-                  <div className="cursor-help pt-[1px]">
-                    <i
-                      className="iconfont iconcheck text-[#ffb503]"
-                      style={{ fontSize: "20px", lineHeight: "20px" }}
-                    ></i>
+                  <div className="flex items-center justify-center gap-1 truncate">
+                    <h3 className="mb-0 mt-0 !h-[32px] overflow-hidden pb-0 text-[22px]">
+                      {dao.name}
+                    </h3>
+                    <div className="cursor-help pt-[1px]">
+                      <i
+                        className="iconfont iconcheck text-[#ffb503]"
+                        style={{ fontSize: "20px", lineHeight: "20px" }}
+                      ></i>
+                    </div>
                   </div>
-                </div>
-                <div className="text-skin-text mb-[12px]">
-                  {dao.members} members
+                  <div className="text-skin-text mb-[12px]">
+                    {dao.members} members
+                  </div>
                 </div>
               </div>
-            </div>
-          </a>
+            </a>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 }
